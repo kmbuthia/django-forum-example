4ea6a56c743596995634452d4864b368454ab894
Kenneth Mbuthia <kmbuthia47@gmail.com>
2021-02-05 11:55:40 +0300

    Setting default port

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webforumproject.settings")
+    runserver.default_port = 8001
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
